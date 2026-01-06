bc46c8b233fd15ebef38a09d6c4eedb69a8910aa
ngernyi <122040135+ngernyi@users.noreply.github.com>
2026-01-06 12:23:42 +0800

    Add for deploy

--- backend/app.py
+++ backend/app.py
@@ -39,5 +39,9 @@ app.register_blueprint(admin_blueprint)
 def index():
     return jsonify({"message": "Backend is running."})
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
+    
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)

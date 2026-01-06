661d0397dab7506c32c8b2e17624e282f79e9640
ngernyi <122040135+ngernyi@users.noreply.github.com>
2026-01-06 23:04:00 +0800

    fix web url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,6 @@
 // config.js
 export const API_BASE_URL = "https://disaster-evacuation-simulation-1.onrender.com";
-export const WEB_URL = "http://localhost:5501/Code";
-// config.js
+export const WEB_URL = "https://disaster-evacuation-simulation-web.onrender.com";
+
+// export const WEB_URL = "http://localhost:5501/Code";
 // export const API_BASE_URL = "http://localhost:5000";
